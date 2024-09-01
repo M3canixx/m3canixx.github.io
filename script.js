1dcf1f5f41b4063aeb4d2e0f7622f6db2b0f5bbe
M3canixx <36899747+M3canixx@users.noreply.github.com>
2024-09-01 02:30:13 +0200

    update scroll

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll('.section-parallax');
-    const navButtons = document.querySelectorAll('.nav-buttons a');
     let currentSection = 0;
+    let isScrolling = false;
 
     function scrollToSection(index) {
+        isScrolling = true;
         sections[index].scrollIntoView({ behavior: 'smooth' });
+        setTimeout(() => {
+            isScrolling = false;
+        }, 500); // Temps pour terminer le scroll
     }
 
     window.addEventListener('wheel', (event) => {
-        if (event.deltaY > 0) {
-            currentSection = Math.min(sections.length - 1, currentSection + 1);
-        } else {
-            currentSection = Math.max(0, currentSection - 1);
-        }
-        scrollToSection(currentSection);
-    });
-
-    navButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            event.preventDefault();
-            const targetId = event.target.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
-            if (targetSection) {
-                targetSection.scrollIntoView({ behavior: 'smooth' });
+        if (!isScrolling) {
+            if (event.deltaY > 0) {
+                currentSection = Math.min(sections.length - 1, currentSection + 1);
+            } else {
+                currentSection = Math.max(0, currentSection - 1);
             }
-        });
+            scrollToSection(currentSection);
+        }
+        event.preventDefault(); // Empêche le scroll par défaut
     });
-
     const audio = document.getElementById('background-music');
     const volumeControl = document.getElementById('volume-control');
 
