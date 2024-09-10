21d127b217c2a5020dce8d4ae16ebe9991a7bcff
M3canixx <36899747+M3canixx@users.noreply.github.com>
2024-09-11 00:37:38 +0200

    update translation

--- langage.js
+++ langage.js
@@ -7,12 +7,12 @@ function switchLanguage() {
     const currentUrl = window.location.href;
     
     // Si c'est la version française, redirige vers la version anglaise
-    if (currentUrl.includes('index_fr.html')) {
+    if (currentUrl.includes('index.html')) {
         window.location.href = 'index_en.html';
     } 
     // Si c'est la version anglaise, redirige vers la version française
     else if (currentUrl.includes('index_en.html')) {
-        window.location.href = 'index_fr.html';
+        window.location.href = 'index.html';
     }
 }
 
