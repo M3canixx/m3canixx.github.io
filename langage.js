46e54739b67a8380d39a6e65e29e7309514d46b0
M3canixx <36899747+M3canixx@users.noreply.github.com>
2024-09-19 20:12:52 +0200

    update lang

--- langage.js
+++ langage.js
@@ -7,7 +7,7 @@ function switchLanguage() {
     const currentUrl = window.location.href;
     
     // Si c'est la version française, redirige vers la version anglaise
-    if (currentUrl.includes('index.html')) {
+    if (not(currentUrl.includes('index_en.html'))) {
         window.location.href = 'index_en.html';
     } 
     // Si c'est la version anglaise, redirige vers la version française
@@ -15,17 +15,4 @@ function switchLanguage() {
         window.location.href = 'index.html';
     }
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const lang = urlParams.get('lang');
-
-    // Si 'lang' est 'en', redirige vers la version anglaise
-    if (lang === 'fr') {
-        window.location.href = 'index_fr.html';
-    }
-    if (lang === 'en') {
-        window.location.href = 'index_en.html';
-    }
-});
 
